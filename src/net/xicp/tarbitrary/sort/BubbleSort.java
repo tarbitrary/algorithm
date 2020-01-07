b880da003baf1bc2c09f50e4b9faf9fe1e2bb46f
tarbitrary <tarbitrary@gmail.com>
2020-01-07 22:34:48 +0800

    冒泡排序代码提交

---
+++ src/net/xicp/tarbitrary/sort/BubbleSort.java
@@ -0,0 +1,36 @@
+package net.xicp.tarbitrary.sort;
+
+import java.util.Arrays;
+
+/**
+ * 冒泡排序
+ * @author tarbitrary
+ */
+public class BubbleSort {
+    public static void sort(int[] toSortArray) {
+         for (int i = 0, len = toSortArray.length; i < len; i ++) {
+             for (int j = len - 1; j > i; j --) {
+                 if (toSortArray[j] < toSortArray[j - 1]) {
+                     swap(toSortArray, j);
+                 }
+             }
+         }
+
+
+    }
+
+    private static void swap(int[] toSortArray, int j) {
+        int tmp = toSortArray[j];
+        toSortArray[j] = toSortArray[j - 1];
+        toSortArray[j - 1] = tmp;
+    }
+
+    public static void main(String[] args) {
+        int a[] = {9, 7, 8, 3, 1, 5, 2};
+        sort(a);
+        Arrays.stream(a).forEach(p -> System.out.print(p + " "));
+
+    }
+
+
+}
