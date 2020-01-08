ff7b8b5b98c96793d0a9ed1197c1a03d876a3d0b
tarbitrary <tarbitrary@gmail.com>
2020-01-08 10:24:05 +0800

    选择排序代码提交

--- src/net/xicp/tarbitrary/graph/Prim.java
+++ src/net/xicp/tarbitrary/graph/Prim.java
@@ -3,14 +3,14 @@ package net.xicp.tarbitrary.graph;
 import java.util.Scanner;
 
 /**
- * <p></p>
+ * <p>普拉姆算法可在加权连通图里搜索最小生成树。意即由此算法搜索到的边子集所构成的树中，不但包括了连通图里的所有顶点（英语：Vertex (graph theory)），且其所有边的权值之和亦为最小</p>
  *
  * @author tarbitrary
  * @version V1.0
  * @created_time 2019-11-04 10:52
  * @module 现货延期模块
  **/
-public class Prime {
+public class Prim {
     private int[] vertexs;
 
     private int[][] edges;
---
+++ src/net/xicp/tarbitrary/sort/SelectionSort.java
@@ -0,0 +1,43 @@
+package net.xicp.tarbitrary.sort;
+
+import java.util.Arrays;
+
+/**
+ * <p>选择排序</p>
+ *
+ * @author tarbitrary
+ * @version V1.0
+ * @created_time 2020-01-08 10:07
+ **/
+public class SelectionSort {
+
+    public static void sort(int[] toSortArray) {
+
+        for (int i = 0, len = toSortArray.length; i < len; i ++) {
+            int min = i;
+
+            for (int j = i + 1; j < len; j++) {
+                if (toSortArray[min] > toSortArray[j]) {
+                    min = j;
+                }
+            }
+
+            swap(toSortArray, i, min);
+        }
+        
+    }
+
+    private static void swap(int[] toSortArray, int j, int k) {
+        int tmp = toSortArray[j];
+        toSortArray[j] = toSortArray[k];
+        toSortArray[k] = tmp;
+    }
+
+    public static void main(String[] args) {
+        int a[] = {9, 7, 8, 3, 1, 5, 2};
+        sort(a);
+        Arrays.stream(a).forEach(p -> System.out.print(p + " "));
+
+    }
+    
+}
