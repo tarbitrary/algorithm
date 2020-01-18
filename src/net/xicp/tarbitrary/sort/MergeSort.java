4040ae1cbee3e68093f2015dcc201189b81035e2
tarbitrary <tarbitrary@gmail.com>
2020-01-18 16:59:43 +0800

    归并排序代码提交（递归＆非递归）

--- src/net/xicp/tarbitrary/sort/MergeSort.java
+++ src/net/xicp/tarbitrary/sort/MergeSort.java
@@ -4,44 +4,107 @@ import java.util.Arrays;
 
 /**
  * 归并排序
+ *
  * @author tarbitrary
  */
 public class MergeSort {
 
-    private static void mergeSort(int[] toSortArray, int[] tmpArr, int startIndex, int endIndex) {
+    private static void mergeSortWithNoRecursion(int[] toSortArray) {
 
-        if (startIndex < endIndex) {
-            int middle = ((endIndex - startIndex) >> 1) + startIndex;
-            mergeSort(toSortArray, tmpArr, startIndex, middle);
-            mergeSort(toSortArray, tmpArr, middle, endIndex);
+        int k = 1;
+        int length = toSortArray.length;
+        int[] mergeArray = new int[toSortArray.length];
 
-
+        while (k <= length) {
+            mergeToArray(toSortArray, mergeArray, k, length);
+            k <<= 1;
+            mergeToArray(mergeArray, toSortArray, k, length);
+            k <<= 1;
         }
-        //TODO
+    }
 
+    private static void mergeToArray(int[] toSortArray, int[] mergeArray, int k, int length) {
+        int i = 0;
+        for (; length >= (i + 2 * k); i += 2*k) {
+            mergeArrayInRange(toSortArray, mergeArray, i, i + k - 1, i + 2*k - 1);
+        }
+
+        if (length >= i + k && length < i + 2 * k) {
+            mergeArrayInRange(toSortArray, mergeArray, i, i + k - 1, length - 1);
+        } else {
+            for (; i < length; i ++) {
+                mergeArray[i] = toSortArray[i];
+            }
+        }
 
 
 
     }
 
-
-
-    private static void swap(int[] toSortArray, int j, int k) {
-        int tmp = toSortArray[j];
-        toSortArray[j] = toSortArray[k];
-        toSortArray[k] = tmp;
+    public static void mergeSortWithRecursion(int[] toSortArray) {
+        mergeSortWithRecursion(toSortArray, toSortArray, 0, toSortArray.length - 1);
     }
+
+    private static void mergeSortWithRecursion(int[] toSortArray, int[] tmpSortArray, int startIndex, int endIndex) {
+        if (startIndex == endIndex) {
+            tmpSortArray[startIndex] = toSortArray[startIndex];
+            return;
+        }
+
+        int middle = (endIndex - startIndex) / 2 + startIndex;
+        int[] toMergeArray = new int[toSortArray.length];
+
+        mergeSortWithRecursion(toSortArray, toMergeArray, startIndex, middle);
+        mergeSortWithRecursion(toSortArray, toMergeArray, middle + 1, endIndex);
+        mergeArrayInRange(toMergeArray, tmpSortArray, startIndex, middle, endIndex);
+
+    }
+
+    private static void mergeArrayInRange(int[] toSortArray, int[] toMergeArray, int startIndex, int middleIndex, int endIndex) {
+        int k, leftStartIndex, rightStartIndex;
+        for (k = startIndex, leftStartIndex = startIndex, rightStartIndex = middleIndex + 1; leftStartIndex <= middleIndex && rightStartIndex <= endIndex; k++) {
+            if (toSortArray[leftStartIndex] < toSortArray[rightStartIndex]) {
+                toMergeArray[k] = toSortArray[leftStartIndex++];
+            } else {
+                toMergeArray[k] = toSortArray[rightStartIndex++];
+            }
+        }
+
+
+        while (leftStartIndex <= middleIndex) {
+            toMergeArray[k++] = toSortArray[leftStartIndex++];
+        }
+
+        while (rightStartIndex <= endIndex) {
+            toMergeArray[k++] = toSortArray[rightStartIndex++];
+        }
+
+
+    }
+
+
 
     public static void main(String[] args) {
-        int a[] = {9, 7, 8, 3, 1, 5, 2};
-        mergeSort(a, a, 0, a.length);
+        int[] a = {9, 7, 8, 3, 1, 5, 2};
+        mergeSortWithRecursion(a);
         Arrays.stream(a).forEach(p -> System.out.print(p + " "));
 
         System.out.println();
 
         int[] b = {10};
-        mergeSort(b, b, 0, a.length);
+        mergeSortWithRecursion(b);
         Arrays.stream(b).forEach(p -> System.out.print(p + " "));
+
+        System.out.println("\n非递归方式实现归并排序");
+        int[] a1 = {9, 7, 8, 3, 1, 5, 2};
+        mergeSortWithNoRecursion(a1);
+        Arrays.stream(a1).forEach(p -> System.out.print(p + " "));
+
+        System.out.println();
+
+        int[] b1 = {10};
+        mergeSortWithNoRecursion(b);
+        Arrays.stream(b1).forEach(p -> System.out.print(p + " "));
 
     }
 
